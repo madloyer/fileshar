a36499b520b4ea929ebd4bfb22e9ca79d14f8487
KingCio <103745710+cioyourfvboynih@users.noreply.github.com>
2022-08-30 00:11:25 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,5 +1,5 @@
 #(©)Codexbotz
-# @ItsMeYons
+# @cioyourfvboy
 
 from pyrogram import __version__
 from bot import Bot
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Dionprastt/protectconten'>Click here</a>\n○ Channel : @File_Sharing_Support\n○ Owner Repo: @ItsMeYons</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/cioyourfvboynih/protectcio'>Click here</a>\n○ Channel : @whycio\n○ Owner Repo: @cioyourfvboy</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
